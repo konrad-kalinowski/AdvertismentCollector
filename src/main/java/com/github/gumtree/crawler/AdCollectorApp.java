6f4231aff21eca798e3c7e490e8b3f5aa2cc4fe5
Konrad Kalinowski <kalinowski.konrad3@gmail.com>
2018-08-30 22:53:09 +0200

    Adverts schedule

--- src/main/java/com/github/gumtree/crawler/AdCollectorApp.java
+++ src/main/java/com/github/gumtree/crawler/AdCollectorApp.java
@@ -7,13 +7,19 @@ import com.github.gumtree.crawler.nominatim.model.AdvertSearchSpec;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 
 @SpringBootApplication
@@ -24,6 +30,8 @@ public class AdCollectorApp implements CommandLineRunner {
     private CompositeAdLinksCollector compositeLinksCollector;
     @Autowired
     private BatchAdInfoCollector batchAdInfoCollector;
+    @Value("${advers.fetcher.delay.seconds:300}")
+    private int advertsFetcherDelay;
 
     public static void main(String[] args) {
         new SpringApplication(AdCollectorApp.class).run(args);
@@ -31,13 +39,19 @@ public class AdCollectorApp implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        AdvertSearchSpec advertSearchSpec = new AdvertSearchSpec("Kraków", "Poland",
-                "https://www.gumtree.pl/s-mieszkania-i-domy-sprzedam-i-kupie/krakow/v1c9073l3200208p1",
-                "https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/krakow/",
-                "https://www.otodom.pl/sprzedaz/mieszkanie/krakow/?search%5Bdist%5D=0&search%5Bsubregion_id%5D=410&search%5Bcity_id%5D=38");
-        AdLinks advertLinks = compositeLinksCollector.getAdvertLinks(advertSearchSpec, 1);
 
-        batchAdInfoCollector.collectAdvertsDetails(advertLinks);
+        Executors.newScheduledThreadPool(1).scheduleWithFixedDelay(() -> {
+
+            log.info("Fetching link adverts started");
+            AdvertSearchSpec advertSearchSpec = new AdvertSearchSpec("Kraków", "Poland",
+                    "https://www.gumtree.pl/s-mieszkania-i-domy-sprzedam-i-kupie/krakow/v1c9073l3200208p1",
+                    "https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/krakow/",
+                    "https://www.otodom.pl/sprzedaz/mieszkanie/krakow/?search%5Bdist%5D=0&search%5Bsubregion_id%5D=410&search%5Bcity_id%5D=38");
+            AdLinks advertLinks = compositeLinksCollector.getAdvertLinks(advertSearchSpec, 1);
+
+            batchAdInfoCollector.collectAdvertsDetails(advertLinks);
+            log.info("Fetching link adverts finished");
+        }, 0, advertsFetcherDelay, TimeUnit.SECONDS);
         try {
             System.out.println("Finish initialization, waiting for key press.");
             System.in.read();
